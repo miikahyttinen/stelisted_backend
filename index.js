0615cfcd8d8272197840a86758ec27f87b90ee2e
Miika Hyttinen <hyttinenmiika@gmail.com>
2019-09-12 15:46:02 +0300

    Better api: fixing data already at back end

--- index.js
+++ index.js
@@ -5,44 +5,35 @@ const middleware = require('./utils/middleware')
 const queryString = require('query-string')
 const fetch = require('node-fetch')
 
+const baseApiUrl = ''
+
 app.use(cors())
 app.use(middleware.requestLogger)
-
-let songs = [
-  {
-    id: 1,
-    name: 'September',
-    artist: 'Earth Wind & Fire'
-  },
-  {
-    id: 2,
-    name: 'Boogie Wonderland',
-    artist: 'Earth Wind & Fire'
-  },
-  {
-    id: 3,
-    name: 'Sleeping In My Car',
-    artist: 'Roxette'
-  }
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>This is Setlisted!</h1>')
 })
 
-app.get('/songs', (req, res) => {
-  res.json(songs)
-})
-
 app.get('/spotify', async (req, res) => {
   const spotifyAccessToken = queryString.parseUrl(req.headers.referer).query
     .access_token
-  const apiResponse = await fetch(
-    `http://api.spotify.com/v1/playlists/1iHADEaVKULre5JnAMAslK?access_token=${spotifyAccessToken}`
-  )
-    .then(res => res.json()) // expecting a json response
-    .then(json => json)
-  res.json(apiResponse)
+  let responseJson = []
+  let next = true
+  let url = `http://api.spotify.com/v1/playlists/1iHADEaVKULre5JnAMAslK/tracks?access_token=${spotifyAccessToken}`
+  while (next) {
+    console.log('URL-->', url)
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log('DATA ------------------- !!!!', data.items)
+    responseJson = responseJson.concat(data.items)
+    if (data.next) {
+      url = `${data.next}&access_token=${spotifyAccessToken}`
+    } else {
+      next = false
+    }
+  }
+  console.log('responseJson ****', responseJson.length)
+  return res.send(responseJson)
 })
 
 const PORT = 3001

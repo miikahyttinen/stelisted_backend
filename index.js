03ff30562c70affd32fb567fc5e072b1d08d7c85
Miika Hyttinen <hyttinenmiika@gmail.com>
2020-02-03 17:41:54 +0200

    Environment variables

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static('build'))
 app.use(middleware.requestLogger)
 app.use(bodyParser.json())
 
-const PORT = 3001 || process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
